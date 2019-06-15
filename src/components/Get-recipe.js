d14093f9f7dd44b8fce155edd6810df218a5560e
MGIngham <matt.g.ingham@gmail.com>
2019-06-15 16:49:05 +0100

    On click

--- src/components/Get-recipe.js
+++ src/components/Get-recipe.js
@@ -6,29 +6,42 @@ class GetRecipe extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            hits: ''
+            hits: []
         }
+
+        this.handleClick = this.handleClick.bind(this);
     }
 
-
-
-    componentDidMount(){
-
+    handleClick(e) {
+        e.preventDefault();
         axios.get("https://api.edamam.com/search?q=bacon,cheese,avocado&app_id=9440941b&app_key=b631529b983cd2f88a9437b1c9e22851&from=0&to=5&calories=591-722&health=alcohol-free")
         .then(response => {
             console.log(response.data);
-            this.setState({ hits: response.data });
+            this.setState({ hits: response.data.hits });
         })
         .catch(error => {
             console.log(error); 
         });
-
-    };
+    }
 
     render() {
-        const { hits } = this.state;
+
         return(
-            <p>{hits}</p>            
+            <div>
+                <button onClick={this.handleClick}>Go</button>
+
+                <br />
+
+                {this.state.hits.map((hit,index) => 
+                <div key={index}>
+                    <h3>
+                        {hit.recipe.label}
+                    </h3>
+                    <li>
+                        {hit.recipe.ingredientLines.map((ingredient, index) => <span key={index}>{ingredient}</span>)}
+                    </li>
+                </div>)}
+            </div>
         );
     }
 }
